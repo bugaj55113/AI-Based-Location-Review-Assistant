9955ed9c10cb2e724f41260dcfa0f2af1dfd8d61
bugaj55113 <bbugi2016@gmail.com>
2023-09-28 12:16:28 +0200

    Added most popular character

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 with open('input/f.txt', 'r') as f:
     data = f.read().lower()
 
+frequency_map = {x: data.count(x) for x in set(data)}
+popular = max(frequency_map, key=frequency_map.get)
+
 keyword = input("Enter your character, word or sentence: ")
 
 r = data.count(keyword)
 
 print(f"Keyword {keyword} has occurred {r} times")
+print(f"The most popular character in the file is {popular}")
